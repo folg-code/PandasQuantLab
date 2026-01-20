2b52ff2f7cc8eaaefbb1df0c291f6846b6f3dc96
filip <filip1folga@gmail.com>
2026-01-20 11:31:19 +0100

    market_data: fully implemented unified core (features, cache, provider)

--- core/live_trading/main.py
+++
@@ -1,8 +0,0 @@
-from core.live_trading.runner import run_live_symbols
-import config
-
-if __name__ == "__main__":
-    # Definicja lookbacków dla różnych TF w formie słownika
-    # Możesz podać liczbę świec lub timedelta w sekundach
-
-    run_live_symbols(config.lookbacks)
